6ceff34a4a3e138fc7c3c1518c714138473be8eb
LIM HEE JIN <xeexin0331@gmail.com>
2023-10-30 02:05:30 +0900

    git test

--- src/VendingMachine.java
+++ src/VendingMachine.java
@@ -1,6 +1,6 @@
 public class VendingMachine {
-    //field
-    //constructor
+    //field  (가지는 것들) 예) 동전통, 상품 케이스
+    //constructor 생성자
     //method
 
 }
--- src/VendingMachineMain.java
+++ src/VendingMachineMain.java
@@ -1,6 +1,6 @@
 public class VendingMachineMain {
     public static void main(String[] args) {
-        System.out.println("HELLO");
+        VendingMachine vm1 = new VendingMachine();
 
     }
 }
